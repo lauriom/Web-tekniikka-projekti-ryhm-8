5a9d30ac3937ece529e5dbf600be5e30b30485c9
lauriom <43406072+lauriom@users.noreply.github.com>
2018-10-09 11:30:01 +0300

    Delete tilaus.sql

--- sql pohjat/tilaus.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tilaukset (
-  tilaus_id int(11) not null AUTO_INCREMENT PRIMARY KEY,
-  user_id int (11) NOT NULL,
-  kukka_id int (11)NOT NULL,
-  tilaus_maara varchar(256) NOT NULL,
-  tilaus_yht varchar(256) NOT NULL
-);
