7132339f21f91e1d43cc2042c6d81bdf1f5ffc3d
lauriom <43406072+lauriom@users.noreply.github.com>
2018-10-10 11:18:25 +0300

    Update singup.php

--- php/singup.php
+++ php/singup.php
@@ -3,8 +3,7 @@ include 'tilaus/tilaus.php';
 ;} else echo '<section>
     <div class="valilehti" id="lomake">
         <img src="kuvat/japflower.png" id="flower" alt="Kukka?">
-        <form class="singup" action="http://localhost:63342/!Kurssiproj/php/singup-inc.php" method="POST">
-            <!--MUUTA OSOITETTA SERVERILLÄ!!!!!!!!-->
+
             <p><strong>Luo tili</strong></p>
             <input type="text" name="etu" placeholder="etunimi">
             <input type="text" name="suku" placeholder="sukunimi">
